0e26a136dfdc1bec6246da09fa3ddd1c57a07ebb
Mel0w0l <95467063+Mel0w0l@users.noreply.github.com>
2021-12-10 01:03:04 +1100

    Day 4_a Final Code

--- Day_4_a.py
+++ Day_4_a.py
@@ -1,21 +1,22 @@
-# Opening input file into a list
+## Opening input file into a list
 input = list()
 with open('Day_4_input.txt') as f:
     for line in f:
         input.append(line)
 
 
-# Isolating the drawn numbers into its own list
-drawn_numbers = list()
+## Isolating the drawn numbers into its own list
+temp_list = list()
 for line in input:
     if ',' in line:
-        drawn_numbers.append(line.replace('\n',''))
+        temp_list.append(line.replace('\n',''))
         input.remove(line)
+        drawn_numbers = temp_list[0].split(",")
+
+#print(drawn_numbers)
 
 
-print(drawn_numbers)
-
-# Making a dictionary with the board_number as the key and the numbers as the value
+## Making a dictionary with the board_number as the key and the numbers as the value
 boards = dict()
 boards_num = 0
 temp_list = list()
@@ -37,7 +38,7 @@ for line in input:
 #print(boards[99])
 #print(boards[100])
 
-# Winning possibilities
+## Winning possibilities
 bingo_positions = [[0,1,2,3,4]
          , [5,6,7,8,9]
          , [10,11,12,13,14]
@@ -53,15 +54,46 @@ bingo_positions = [[0,1,2,3,4]
          ]
 
 checking_bingo = dict()
-#for i in range(1,max(boards.keys()) + 2):
-#    print(i)
-#    print(boards[i])
+for i in range(1,max(boards.keys()) + 1):
+    checking_bingo[i] = list()
 
-#for number in drawn_numbers:
-#    for i in range(1,5):
-#        print(i)
-#        for num in boards[i]:
-#            if num == number:
-#                then
-#            print(boards[i])
-#            print(num)
+#    checking_bingo[i].append(i)
+
+winning_board = 0
+winning_board_marked_pos = 0
+
+for number in drawn_numbers:
+    if winning_board == 0:
+    #    print(number)
+        for i in range(1,max(boards.keys()) + 1):
+            for num in boards[i]:
+                if num == number:
+                    checking_bingo[i].append(boards[i].index(num))
+    #            print(checking_bingo[i])
+                for j in range(len(bingo_positions)):
+                    result = all(elem in checking_bingo[i] for elem in bingo_positions[j])
+                    if result:
+                        winning_board = i
+                        winning_board_marked_pos = checking_bingo[i]
+                        break
+                    else:
+                        continue
+    else:
+        break
+
+print(winning_board)
+print(winning_board_marked_pos)
+print(boards[winning_board])
+
+score = 0
+for num in boards[winning_board]:
+#    print(num)
+    if boards[winning_board].index(num) not in winning_board_marked_pos:
+        score += int(num)
+#        print(score)
+
+last_num = boards[winning_board][int(winning_board_marked_pos[len(winning_board_marked_pos)-1])]
+#print(last_num)
+
+final_score = score * int(last_num)
+print('Final Score: ' + str(final_score))
