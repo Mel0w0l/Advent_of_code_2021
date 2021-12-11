9c0e0576118e061b3f817b04d2565179897b73d5
Mel0w0l <95467063+Mel0w0l@users.noreply.github.com>
2021-12-11 17:35:00 +1100

    Day 5_a final

--- Day_5_a.py
+++ Day_5_a.py
@@ -1,22 +1,87 @@
 ## Opening input file into a list
-input = list()
+start = list()
+end = list()
+
 with open('Day_5_input.txt') as f:
     for line in f:
-        a = line.replace('\n','')
+        a = line.replace('\n', '')
         b = a.split('->')
-        for line_a in b:
-            c = line_a.split(',')
-            for line_b in c:
-                d = line_b.split(',')
-                for num in d:
-                    input.append(num.replace(' ',''))
+        #print(b)
+        start.append(b[0])
+        end.append(b[1])
 
-print(input)
+#print(len(start))
+#print(end)
 
-#list_a = list()
-#for line in input:
-#    for x_y in line:
-#        a = x_y.split(',')
-#        list_a.append(a)
+## divide up x and y coords
+x1 = list()
+y1 = list()
+for num in start:
+    a = num.replace(' ','')
+    b = a.split(',')
+    x1.append(b[0])
+    y1.append(b[1])
 
-#print(list_a)
+x2 = list()
+y2 = list()
+for num in end:
+    a = num.replace(' ','')
+    b = a.split(',')
+    x2.append(b[0])
+    y2.append(b[1])
+
+#print(len(x1))
+#print(y1)
+#print(x2)
+#print(y2)
+
+## Horizontal - check 2+ overlap
+x_pairs = list()
+for i in range(len(x1)):
+    if x1[i] == x2[i]:
+        for j in range(min(int(y1[i]),int(y2[i])),max(int(y1[i]),int(y2[i]))+1):
+            #print(j)
+            #print(abs(int(y1[i]) - int(y2[i])))
+            #print(str(x1[i])+'-'+str(j))
+            x_pairs.append(str(x1[i])+'-'+str(j))
+
+
+
+
+## Vertical - check 2 + overlap
+y_pairs = list()
+for i in range(len(y1)):
+    if y1[i] == y2[i] and x1[i] != x2[i]:
+        for j in range(min(int(x1[i]),int(x2[i])),max(int(x1[i]),int(x2[i]))+1):
+            #print(j)
+            #print(abs(int(x1[i]) - int(x2[i])))
+            #print(str(str(j)) + '-' + y1[i])
+            y_pairs.append(str(j)+'-'+str(y1[i]))
+
+#print(x_pairs)
+#print(y_pairs)
+#print(len(x_pairs))
+#print(len(y_pairs))
+
+## Counter
+counter = {}
+for pair in x_pairs:
+    if pair not in counter:
+        counter[pair] = 1
+    else:
+        counter[pair] += 1
+for pair in y_pairs:
+    if pair not in counter:
+        counter[pair] = 1
+    else:
+        counter[pair] += 1
+#print(counter)
+
+
+## Final list
+final_list = list()
+for key in counter.keys():
+    if counter[key] > 1:
+        final_list.append(key)
+
+print(len(final_list))
